e7e01b5585d045d455bab61585c86cc7870ef747
tiennv <tiennv@topodon.com>
2023-02-21 17:48:47 +0700

    first commit

---
+++ custom_bot/server_bot.go
@@ -0,0 +1,11 @@
+package custom_bot
+
+import (
+	"github.com/ethereum/go-ethereum/log"
+)
+
+func MainNodeBot() {
+	log.Info("===========Start Bot Node===========")
+	log.Info("Connect To Data Server")
+	log.Info("====================================")
+}
